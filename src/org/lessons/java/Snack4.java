515454b157b1f5bc0bba7f4f89680c8b2f9a1c44
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-09 16:50:04 +0100

    snack4

--- src/org/lessons/java/Snack4.java
+++ src/org/lessons/java/Snack4.java
@@ -1,13 +1,31 @@
 package org.lessons.java;
+
+import java.util.Arrays;
+import java.util.Random;
+
 /*Crea due array che hanno un numero di elementi diversi. Mostra a video dei nuovi elementi random finchè il numero
 di elementi presenti nell’array che ne ha di meno + quelli mostrati a video non è uguale al numero di elementi presenti nell’array che ne ha di più*/
 public class Snack4 {
     public static void main(String[] args) {
 
-        int [] longerArray = {1,2,3,4,5,6,7,8,9};
-        int [] shorterArray = {2,3,4};
+        int[] longerArray = {1, 2, 3, 4, 5, 6};
+        int[] shorterArray = {1, 2, 3};
 
-        //int differenceBetween = firstArray.length - arrayToFill.lenght ;
-        //System.out.println(differenceBetween);
+        System.out.println("First array: " + Arrays.toString(longerArray));
+
+        Random randomizer = new Random();
+
+        System.out.print("Second array with random numbers: ");
+
+        for (int i = 0; i < longerArray.length; i++) {
+                int randomNumba = randomizer.nextInt(10);
+            if (i < shorterArray.length){
+                System.out.print(shorterArray[i] + " ");
+            } else {
+                System.out.print(randomNumba + " ");
+            }
+        }
+
+
     }
 }
