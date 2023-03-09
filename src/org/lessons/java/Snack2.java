7850d9a6a908a20076a8cdd69595e05de2b73f30
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-09 15:44:58 +0100

    fixed:snack2

--- src/org/lessons/java/Snack2.java
+++ src/org/lessons/java/Snack2.java
@@ -1,13 +1,20 @@
 package org.lessons.java;
+
+import java.util.Random;
+
 //Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
 public class Snack2 {
     public static void main(String[] args) {
-        String[] surnames = { "Tulipano", "Gratta", "Bonuccia"};
-        String[] names = {"Franco", "Marco", "Laura"};
+        String[] surnames = { "Tulipano", "Gratta", "Bonuccia", "Fasullo", "Ligresti"};
+        String[] names = {"Franco", "Marco", "Laura", "Marina"};
+
+        Random randomizer = new Random();
 
 
-        for (int i = 0; i < surnames.length && surnames.length == names.length ; i++){
-            System.out.println(surnames[i] + " " + names[i]);
+        for (int i = 0; i < 10 ; i++){
+            int randomNames = randomizer.nextInt(names.length-1 );
+            int randomSurnames = randomizer.nextInt(surnames.length-1);
+            System.out.println(surnames[randomSurnames] + " " + names[randomNames]);
         }
     }
 }
---
+++ src/org/lessons/java/Snack4.java
@@ -0,0 +1,13 @@
+package org.lessons.java;
+/*Crea due array che hanno un numero di elementi diversi. Mostra a video dei nuovi elementi random finchè il numero
+di elementi presenti nell’array che ne ha di meno + quelli mostrati a video non è uguale al numero di elementi presenti nell’array che ne ha di più*/
+public class Snack4 {
+    public static void main(String[] args) {
+
+        int [] longerArray = {1,2,3,4,5,6,7,8,9};
+        int [] shorterArray = {2,3,4};
+
+        //int differenceBetween = firstArray.length - arrayToFill.lenght ;
+        //System.out.println(differenceBetween);
+    }
+}
