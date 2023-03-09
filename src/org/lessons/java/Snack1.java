3d1532e0e264ce0eb95c3addf42922a66a6939a6
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-09 14:30:57 +0100

    First snack

---
+++ src/org/lessons/java/Snack1.java
@@ -0,0 +1,16 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+public class Snack1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Add a number: ");
+        int num = Integer.parseInt(scan.nextLine());
+
+        int finalNumber = num % 2 == 0 ? num : num + 1;
+
+        System.out.println(finalNumber);
+    }
+}
