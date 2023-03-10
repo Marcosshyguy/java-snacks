cb9b7818ffd532049e262e7c44c7930ddc71ad1e
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-10 09:56:07 +0100

    Fixed: Snack2

--- src/org/lessons/java/Snack2.java
+++ src/org/lessons/java/Snack2.java
@@ -1,5 +1,6 @@
 package org.lessons.java;
 
+import java.util.Arrays;
 import java.util.Random;
 
 //Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
@@ -10,11 +11,16 @@ public class Snack2 {
 
         Random randomizer = new Random();
 
+        int numberOfGuests = 10;
+        String[] guest = new String[numberOfGuests];
 
-        for (int i = 0; i < 10 ; i++){
-            int randomNames = randomizer.nextInt(names.length-1 );
-            int randomSurnames = randomizer.nextInt(surnames.length-1);
-            System.out.println(surnames[randomSurnames] + " " + names[randomNames]);
+        for (int i = 0; i < guest.length ; i++){
+            int randomNames = randomizer.nextInt(names.length);
+            int randomSurnames = randomizer.nextInt(surnames.length);
+            String completeName = surnames[randomSurnames] + " " + names[randomNames];
+            guest[i] = completeName;
         }
+
+        System.out.println(Arrays.toString(guest));
     }
 }
--- src/org/lessons/java/Snack4.java
+++ src/org/lessons/java/Snack4.java
@@ -25,7 +25,5 @@ public class Snack4 {
                 System.out.print(randomNumba + " ");
             }
         }
-
-
     }
 }
